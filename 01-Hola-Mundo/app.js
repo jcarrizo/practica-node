3aa8b4cd3012b508e1723b388845d90bc3a006bf
jcarrizo <jcarrizo15@gmail.com>
2022-02-24 18:32:47 -0300

    mas practicas

--- 01-Hola-Mundo/app.js
+++ 01-Hola-Mundo/app.js
@@ -1,2 +1,4 @@
-let data = 'Hola Mundo'
+let data = 'Hola Mundo';
 console.log(data);
+
+//En la consola 'node 01-Hola-Mundo/app.js' para correr el proyecto con node y asi hasta usar nodemon.
---
+++ 02-Funcion-Flecha/app.js
@@ -0,0 +1,4 @@
+const Saludar = (nombre) => {
+  return `Hola ${nombre}`; // 'Saludos + nombre'
+}
+console.log(Saludar('Jesús'));
