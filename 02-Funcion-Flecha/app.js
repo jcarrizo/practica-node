5091d5813394489796a1e0e4f381d7a4378e3ce5
jcarrizo <jcarrizo15@gmail.com>
2022-03-09 16:02:27 -0300

    .

--- 02-Funcion-Flecha/app.js
+++ 02-Funcion-Flecha/app.js
@@ -2,3 +2,14 @@ const Saludar = (nombre) => {
   return `Hola ${nombre}`; // 'Saludos + nombre'
 }
 console.log(Saludar('Jesús'));
+
+
+// const Sumar = (a, b) => {
+//   return a + b;
+// }
+
+const Sumar = (a, b) => a + b;
+
+
+
+console.log(Sumar(10, 5));
